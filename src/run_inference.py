c5ecac698c9ce753f30b911a1378db9ef9062605
Eric McCalla <ejmccalla@gmail.com>
2025-07-19 19:56:53 -0500

    sync PI dev

--- src/run_inference.py
+++ src/run_inference.py
@@ -1,18 +1,18 @@
-import cv2 
-from depthai_sdk.visualize.visualizer_helper import FramePosition, VisualizerHelper
-from depthai_sdk import OakCamera
+import cv2
 import depthai as dai
-from depthai_sdk.classes import SpatialBbMappingPacket, DetectionPacket
-from depthai_sdk.visualize.visualizer import Visualizer
+import time
+from depthai_sdk import OakCamera, RecordType
+from depthai_sdk.classes import DetectionPacket
 
 with OakCamera(usb_speed=dai.UsbSpeed.HIGH) as oak:
-    color = oak.create_camera('color')
+    color = oak.camera(source='color', resolution='1080p', fps=30)
+    color.config_color_camera(isp_scale=(2, 3)) # 720P
+    # oak.stereo(
+    #     left=oak.camera(source='left', resolution='480p'),
+    #     right=oak.camera(source='right', resolution='480p')      
+    #     )
 
-    # # List of models that are supported out-of-the-box by the SDK:
-    # # https://docs.luxonis.com/projects/sdk/en/latest/features/ai_models/#sdk-supported-models
-    # nn = oak.create_nn('yolov5n_coco_416x416', color, spatial=True)
-
-    nn = oak.create_nn('./models/yolov8n_2025-05-17_00-11-26.json', color, nn_type='yolo', spatial=True)
+    nn = oak.create_nn('./models/yolov5n.json', color, nn_type='yolo', spatial=True)
 
     nn.config_spatial(
         bb_scale_factor=0.5, # Scaling bounding box before averaging the depth in that ROI
@@ -22,11 +22,27 @@ with OakCamera(usb_speed=dai.UsbSpeed.HIGH) as oak:
         calc_algo=dai.SpatialLocationCalculatorAlgorithm.AVERAGE
     )
 
-    def cb(packet: DetectionPacket):
-        print("==========================================================")
-        for det in packet.img_detections.detections:
-            print(f"label={det.label}, x={det.spatialCoordinates.x}, y={det.spatialCoordinates.y}, z={det.spatialCoordinates.z}")
+    # def cb(packet: DetectionPacket):
+    #     for det in packet.img_detections.detections:
+    #         print("==========================================================")
+    #         print(
+    #             f"label={det.label}, "
+    #             f"x={det.spatialCoordinates.x}, "
+    #             f"y={det.spatialCoordinates.y}, "
+    #             f"z={det.spatialCoordinates.z}"
+    #             )
 
-    oak.visualize([nn.out.passthrough], fps=True)
-    # oak.callback(nn.out.passthrough, callback=cb)
+    def cb(packet: DetectionPacket):
+        visualizer = packet.visualizer
+        frame = visualizer.draw(packet.frame)
+        cv2.imshow('Visualizer', frame)
+        # video_writer.write(resized_frame)
+
+    # vis = oak.visualize(nn.out.passthrough, fps=True)
+    # vis.detections().text(auto_scale=True, font_scale=0.5, font_thickness=1)
+
+    # oak.visualize(nn.out.passthrough, fps=True, callback=cb).detections().text(auto_scale=False, font_position=22, font_scale=0.4, font_thickness=1) 
+    oak.record([color.out.encoded], '/media/ejmccalla/RECORDINGS/Match_1/', RecordType.VIDEO)
+    #oak.record([color], '/media/ejmccalla/RECORDINGS/Match_1/', RecordType.VIDEO)
+    
     oak.start(blocking=True)

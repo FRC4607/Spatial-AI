428d8feacc13f7c81cbfe3cfbf92b7ac07abab5b
Eric McCalla <ejmccalla@gmail.com>
2025-07-30 12:16:06 -0500

    latest inference testing

--- src/run_inference.py
+++ src/run_inference.py
@@ -3,16 +3,19 @@ import depthai as dai
 import time
 from depthai_sdk import OakCamera, RecordType
 from depthai_sdk.classes import DetectionPacket
+from depthai_sdk.visualize.configs import TextPosition
 
 with OakCamera(usb_speed=dai.UsbSpeed.HIGH) as oak:
     color = oak.camera(source='color', resolution='1080p', fps=30)
-    color.config_color_camera(isp_scale=(2, 3)) # 720P
+    color.config_color_camera(isp_scale=(2, 5))
     # oak.stereo(
     #     left=oak.camera(source='left', resolution='480p'),
     #     right=oak.camera(source='right', resolution='480p')      
     #     )
 
-    nn = oak.create_nn('./models/yolov5n.json', color, nn_type='yolo', spatial=True)
+    # nn = oak.create_nn('./models/yolov5n.json', color, nn_type='yolo', spatial=True)
+    # nn = oak.create_nn('./models/yolov5n_2025-07-21_01-54-59.json', color, nn_type='yolo', spatial=True)
+    nn = oak.create_nn('./models/yolov5n_2025-07-25_15-28-56.json', color, nn_type='yolo', spatial=True)
 
     nn.config_spatial(
         bb_scale_factor=0.5, # Scaling bounding box before averaging the depth in that ROI
@@ -32,22 +35,29 @@ with OakCamera(usb_speed=dai.UsbSpeed.HIGH) as oak:
     #             f"z={det.spatialCoordinates.z}"
     #             )
 
-    def cb(packet: DetectionPacket):
-        visualizer = packet.visualizer
-        frame = visualizer.draw(packet.frame)
-        cv2.imshow('Visualizer', frame)
-        # video_writer.write(resized_frame)
+    # def cb(packet: DetectionPacket):
+    #     frame = packet.frame
+    #     # visualizer = packet.visualizer
+    #     # frame = visualizer.draw(packet.frame)
+    #     cv2.imshow('Visualizer', frame)
+
 
     # vis = oak.visualize(nn.out.passthrough, fps=True)
     # vis.detections().text(auto_scale=True, font_scale=0.5, font_thickness=1)
-
     # oak.visualize(nn.out.passthrough, fps=True, callback=cb).detections().text(auto_scale=False, font_position=22, font_scale=0.4, font_thickness=1) 
-    oak.record([color.out.encoded], '/media/ejmccalla/RECORDINGS/Match_1/', RecordType.VIDEO)
-    #oak.record([color], '/media/ejmccalla/RECORDINGS/Match_1/', RecordType.VIDEO)
     
+    vis = oak.visualize(nn, fps=True)
+
+    # # Setup the recording
+    # oak.record(
+    #     [color.out.encoded],
+    #     f"/media/ejmccalla/RECORDINGS",
+    #     RecordType.VIDEO
+    # )
+
     oak.start()
     start_time = time.monotonic()
     while oak.running():
-        if time.monotonic() - start_time > 5:
+        if time.monotonic() - start_time > 60:
             break
         oak.poll()

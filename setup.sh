8e82b0d240add690250a24046def218fa03449ef
ejmccalla <ejmccalla@gmail.com>
2025-10-17 20:35:33 -0500

    fix future warning suppression

--- setup.sh
+++ setup.sh
@@ -19,17 +19,7 @@ pip install -e "${workspace}"
 # Install precommit
 pip install pre-commit
 
-
-# Detect site-packages inside the active venv
-SITE_PACKAGES=$(python -c "import site; print([p for p in site.getsitepackages() if 'site-packages' in p][0])")
-CUSTOM_FILE="$SITE_PACKAGES/sitecustomize.py"
-
-# And disable future warnings
-cat <<EOF > "$CUSTOM_FILE"
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
-EOF
-
 # Vscode exports
+export PYTHONWARNINGS="ignore::FutureWarning:depthai_sdk.previews"
 export PYTHONPATH=$workspace
 export PYTHONDONTWRITEBYTECODE=1
